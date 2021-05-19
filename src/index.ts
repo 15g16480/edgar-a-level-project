0710f1a44ad00b9bf92b43d43a753f7e89c05169
Edgar <15g16480@marling.school>
2021-05-19 09:14:55 +0100

    added translate to move camera and started on playerGrounded

--- src/index.ts
+++ src/index.ts
@@ -10,30 +10,30 @@ var Engine = Matter.Engine,
 let sketch = function (p: p5) {
     // create an engine
     let engine: Matter.Engine;
-    let boxA: Matter.Body;
+    let player: Matter.Body;
     let boxB: Matter.Body;
     let groundA: Matter.Body;
     let groundB: Matter.Body;
     let groundC: Matter.Body;
     let finish: Matter.Body;
-    let camera: any;
     let MOVE_LEFT: Matter.Vector;
+    let pg: any;
+    const LEVEL_WIDTH = 10000;
+    let playerGrounded:any;
 
     p.setup = function () {
-        MOVE_LEFT = Matter.Vector.create(-1,0)
-        p.createCanvas(p.windowWidth + 50, p.windowHeight + 50);
+        p.frameRate(60);
+        p.createCanvas(1423, 800);
         engine = Engine.create();
         // create two boxes and a ground
-        boxA = Bodies.rectangle(400, 200, 80, 80, { inertia: Infinity, friction: 0.002 });
-        boxB = Bodies.rectangle(400, 250, 80, 80);
-        groundA = Bodies.rectangle(400, 410, 810, 60, { isStatic: true });
-        groundB = Bodies.rectangle(800, 670, 600, 60, { isStatic: true });
-        groundC = Bodies.rectangle(1600, 670, 810, 60, { isStatic: true });
-        //finish = Bodies.circle(800, 800, 20, { isStatic: true });
-        Matter.Body.setMass(boxA, 4)
-        //let jumping = true;
-        World.add(engine.world, [boxA, boxB, groundA, groundB, groundC/*, finish*/]);
-        Matter.Body.applyForce(boxB, {x: boxB.position.x, y: boxB.position.y}, {x: 0.05, y: 0});
+        player = Bodies.rectangle(400, 250, 40, 40, { inertia: Infinity, friction: 0.002 });
+        boxB = Bodies.rectangle(400, 100, 40, 40);
+        groundA = Bodies.rectangle(400, 410, 810, 30, { isStatic: true });
+        groundB = Bodies.rectangle(800, 670, 600, 30, { isStatic: true });
+        groundC = Bodies.rectangle(1600, 670, 810, 30, { isStatic: true });
+        finish = Bodies.circle(800, 700, 20, { isStatic: true });
+        Matter.Body.setMass(player, 4)
+        World.add(engine.world, [player, boxB, groundA, groundB, groundC, finish]);
     };
     //     }
     //   }
@@ -41,6 +41,7 @@ let sketch = function (p: p5) {
         Engine.update(engine, 30);
         p.background(0);
         p.fill("green");
+        p.translate(-player.position.x + innerWidth/2, 0/*-player.position.y + innerHeight/2*/)
 
         // Draw all bodies
         // p5 and matter js meeting
@@ -51,54 +52,45 @@ let sketch = function (p: p5) {
             })
             p.endShape(p.CLOSE);
         });
+        //check if grouded
+        if ((player.position.y == groundA.position.y +30) /*&& player.position.x>410 || player.position.x>1220*/){
+            playerGrounded = true
+        }
 
         //W
-        if (p.keyIsDown(87) && boxA) {
-            Matter.Body.applyForce(boxA, boxA.position, { x: 0, y: -0.005 });
+        if (p.keyIsDown(87) && playerGrounded == true) {
+            Matter.Body.applyForce(player, player.position, { x: 0, y: -0.02 });
         }
         //A
         if (p.keyIsDown(65)) {
-            Matter.Body.applyForce(boxA, boxA.position, { x: -0.002, y: 0 });
+            Matter.Body.applyForce(player, player.position, { x: -0.002, y: 0 });
         }
         //D
         if (p.keyIsDown(68)) {
-            Matter.Body.applyForce(boxA, boxA.position, { x: +0.002, y: 0 });
+            Matter.Body.applyForce(player, player.position, { x: +0.002, y: 0 });
         }
         //S
         if (p.keyIsDown(83)) {
-            Matter.Body.applyForce(boxA, boxA.position, { x: 0.00, y: 0.005 });
+            Matter.Body.applyForce(player, player.position, { x: 0.00, y: 0.005 });
         }
 
 
-        /*Matter.Events.on(engine, 'collisionStart', function(event) {
-            // We know there was a collision so fetch involved elements ...
-            var aElm = document.getElementById(event.pairs[0].bodyA.elementId);
-            var bElm = document.getElementById(event.pairs[0].bodyB.elementId);
-            // Now do something with the event and elements ... your task ;-)
-        });
-        Matter.Detector.collisions({boxA, groundA}, {boxA, groundB}, {boxA, groundC}, engine)*/
-        /*function touchStarted(){
-        if (boxA == finish)
-            console.log('finished')
-        }*/
-
     }
-    
+
     p.keyPressed = function () {
-       //this changes gravity on toggle of E 
-       if (p.keyCode == 69) {
+        //this changes gravity on toggle of E 
+        if (p.keyCode == 69) {
             engine.world.gravity.y *= -1;
         }
         //fullscreens on P
-        if(p.keyCode == 80) {
+        if (p.keyCode == 80) {
             let fs = p.fullscreen();
-           p.fullscreen(!fs);
+            p.fullscreen(!fs);
         }
-    }
-    function scrollWrapper(x: any, y: any){
-        var wrapper = document.getElementById('wrapper');  
-        wrapper.scrollTop = x;
-        wrapper.scrollLeft = y;
+        //R to reset position
+        /*if (p.keyIsDown(71)) {
+            p.reset();
+        }*/
     }
 };
 
