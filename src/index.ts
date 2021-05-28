ad72ebe2d1e5f4a50b8b40159d10cc1da8a1b69f
Edgar <15g16480@marling.school>
2021-05-28 11:37:23 +0100

    fixing power up system

--- src/index.ts
+++ src/index.ts
@@ -23,25 +23,34 @@ let sketch = function (p: p5) {
     let buttonCD = 0;
     let hasGrav = false;
     let hasSJump = false;
-    let lives = 5
-
+    let lives = 5;
 
     p.setup = function () {
         p.frameRate(60);
         p.createCanvas(1423, 800);
         engine = Engine.create();
+        //render.options.wireframes = false;
         // create two boxes and a ground
-        player = Bodies.rectangle(400, 250, 40, 40, { inertia: Infinity, friction: 0.002 });
+        player = Bodies.rectangle(400, 250, 40, 40, { inertia: Infinity, friction: 0.002, render: {
+
+            sprite: {
+                texture: 'src/Player.png',
+                xScale: 2,
+                yScale: 2
+            }
+        }});
         /*boxB = Bodies.rectangle(400, 100, 40, 40);*/
         groundA = Bodies.rectangle(400, 410, 810, 10, { isStatic: true });
         groundB = Bodies.rectangle(800, 670, 600, 30, { isStatic: true });
         groundC = Bodies.rectangle(1600, 670, 810, 30, { isStatic: true });
         groundD = Bodies.rectangle(1600, 730, 810, 30, { isStatic: true });
-        gravPower = Bodies.circle(800, 620, 20, { isStatic: true });
-        sjumpPower = Bodies.circle(900, 620, 20, { isStatic: true });
+        gravPower = Bodies.circle(800, 630, 20, { isStatic: true });
+        sjumpPower = Bodies.circle(900, 630, 20, { isStatic: true});
+        sjumpPower.isSensor = true
+        gravPower.isSensor = true
         Matter.Body.setMass(player, 4)
         World.add(engine.world, [player, /*boxB,*/ groundA, groundB, groundC, groundD, gravPower, sjumpPower]);
-
+        
     };
     //     }
     //   }
@@ -80,24 +89,32 @@ let sketch = function (p: p5) {
         else {
             playerGrounded = false
         }
+        if (player.position.x < 20){
+            Matter.Body.translate(player,{ x:0, y: 20});
+        }
         //Gravity power
         let collisionGrav = SAT.collides(player, gravPower);
-        if (collisionGrav.collided && p.keyIsDown(81)) {
-            hasGrav = true
-            Matter.Composite.remove(engine.world, gravPower)
-        }
-        else if (collisionGrav.collided && p.keyIsDown(81) && hasSJump == true) {
-            hasGrav = true
-            Matter.Composite.add(engine.world, sjumpPower)
-        }
         let collisionSJump = SAT.collides(player, sjumpPower);
-        if (collisionSJump.collided && p.keyIsDown(81)) {
-            hasSJump = true
-            Matter.Composite.remove(engine.world, sjumpPower)
+        
+        if ((collisionGrav.collided && p.keyIsDown(81)) && hasSJump == true) {
+            console.log('B')
+            hasGrav = true;
+            hasSJump = false;
+            World.add(engine.world, sjumpPower);
         }
-        else if (collisionSJump.collided && p.keyIsDown(81) && hasGrav == true) {
-            hasSJump = true
-            Matter.Composite.add(engine.world, gravPower)
+        if (collisionGrav.collided && p.keyIsDown(81)) {
+            console.log('A')
+            hasGrav = true;
+            Matter.Composite.remove(engine.world, gravPower);
+        }
+        if (collisionSJump.collided && p.keyIsDown(81)) {
+            hasSJump = true;
+            Matter.Composite.remove(engine.world, sjumpPower);
+        }
+        if ((collisionSJump.collided && p.keyIsDown(81)) && hasGrav == true) {
+            hasSJump = true;
+            hasGrav = false;
+            World.add(engine.world, gravPower);
         }
         //W
         if (p.keyIsDown(87) && playerGrounded == true) {
@@ -126,7 +143,7 @@ let sketch = function (p: p5) {
             engine.world.gravity.y *= -1;
         }
         //super jump
-        if (p.keyCode == 87 && hasSJump == true) {
+        if (p.keyCode == 87 && hasSJump == true && playerGrounded == true) {
             Matter.Body.applyForce(player, player.position, { x: 0, y: -0.1 });
         }
         //fullscreens on P
@@ -136,7 +153,7 @@ let sketch = function (p: p5) {
         }
         //R to reset position
         if (p.keyIsDown(71)) {
-            //player.position.x = spawn, player.position.y = spawn ;
+            Matter.Body.translate(player,{ x:0, y: 600});
         }
     }
 };
