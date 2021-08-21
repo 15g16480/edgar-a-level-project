86dd8d5177cb557a2f41041fee457e399a3cd063
Edgar <15g16480@marling.school>
2021-08-21 11:12:56 +0100

    checkpoints causing trouble, it wont become a circle or a sensor, will do checkpoint respawning

--- src/Checkpoints.ts
+++ src/Checkpoints.ts
@@ -6,7 +6,7 @@ class Checkpoint extends GameObject {
     update(): void {}
 
     constructor(s: p5, engine: Matter.Engine, posX: number, posY: number, radius: number, colour: string) {
-        super(s, engine, Bodies.circle(posX, posY, radius, { isStatic: true }), colour);
+        super(s, engine, Bodies.circle(posX, posY, radius, { isStatic: true , isSensor: true}), colour);
         //super(s, engine, Bodies.circle(1400, 440, 20, { isStatic: true }), 'green');
     }
 }
